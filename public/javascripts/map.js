b3bbe2df44a163f1127d037c27d97b4d57708471
Kitaozao <kito_and@hotmail.com>
2016-10-26 14:10:39 -0200

    alterações no pino do mapa ( ainda não esta funcionando 100%)

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,11 +8,22 @@ function initMap() {
         center: myLatlng
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
+    var isDraggable=false;
     var marker = new google.maps.Marker({
         position: myLatlng,
         map: map,
-        draggable:true,
+        draggable:isDraggable,
         title:"Drag me!"
+    });
+    var setDraggable=function () {
+        isDraggable=true;
+        console.log(isDraggable)
+    };
+    var contentString = '<div id="content"> <button onclick="setDraggable();">VAI FILHÃO</button> </div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
     });
 }
