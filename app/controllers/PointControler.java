3fd07b7a05fb1a960cf95c7b6192889a299a39e0
MHI <mauricio_ishida@hotmail.com>
2017-03-08 11:21:07 -0300

    NÂO

--- app/controllers/PointControler.java
+++ app/controllers/PointControler.java
@@ -6,7 +6,7 @@ import play.mvc.*;
  * Created by mhi on 09/12/16.
  */
 public class PointControler extends Controller{
-    public Result salvaponto(){
+    public static Result salvaponto(){
         return ok("Recebi");
     }
 }
